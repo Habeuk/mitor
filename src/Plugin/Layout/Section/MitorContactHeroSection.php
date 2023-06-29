46683a7bbb2a6dfcc700cf6e03a069cc67e337d6
kouwa stephane <kksasteph888@gmail.com>
2023-06-29 18:45:58 +0200

    Maj

--- src/Plugin/Layout/Section/MitorContactHeroSection.php
+++ src/Plugin/Layout/Section/MitorContactHeroSection.php
@@ -8,6 +8,7 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 
 /**
  * Mitor MitorContactHeroSection section php
+ *
  * @Layout(
  *  id = "mitor_contact_hero_section",
  *  label = @Translation("mitor contact hero"),
@@ -29,72 +30,72 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *  }
  * )
  */
-
-class MitorContactHeroSection extends FormatageModelsSection
-{
-
-    /**
-     * {@inheritdoc}
-     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
-     */
-    public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager)
-    {
-        // TODO auto-generated method stub
-        parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'mitor') . "/icons/sections/mitor-contact-hero.png");
-    }
-
-    /**
-     * {@inheritdoc}
-     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::build
-     */
-    public function build(array $regions)
-    {
-        // TODO auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-
-        return $build;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        return parent::defaultConfiguration() + [
-            'css' => '',
-            'load_library' => true,
-            'content' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' =>'Contact hero containt',
-                    'loader' => 'static',
-                ],
-                'fields' => [
-                    'mitor_title' => [
-                        'text' => [
-                            'label' => 'Title',
-                            'value' => 'contact',
-                        ]
-                    ],
-                    'mitor_element' => [
-                        'url' => [
-                            'label' => 'element',
-                            'value' => [
-                                'class' => '',
-                                'text' => 'Home'
-                            ]
-                        ]
-                    ],
-                    'mitor_background_image' => [
-                        'text_html' => [
-                            'label' => 'background image',
-                            'value' => 'http://slidesigma.com/themes/html/mitor/assets/img/coming-soon/banner-3.jpg',
-                        ]
-                    ],
-                ],
-            ],
-        ];
-    }
+class MitorContactHeroSection extends FormatageModelsSection {
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
+   */
+  public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager) {
+    // TODO auto-generated method stub
+    parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'mitor') . "/icons/sections/mitor-contact-hero.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build
+   */
+  public function build(array $regions) {
+    // TODO auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'css' => '',
+      'load_library' => true,
+      'region_css_mitor_title' => 'h2',
+      'content' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Contact hero containt',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'mitor_title' => [
+            'text' => [
+              'label' => 'Title',
+              'value' => 'contact'
+            ]
+          ],
+          'mitor_element' => [
+            'url' => [
+              'label' => 'element',
+              'value' => [
+                'class' => '',
+                'text' => 'Home'
+              ]
+            ]
+          ],
+          'mitor_background_image' => [
+            'text_html' => [
+              'label' => 'background image',
+              'value' => 'http://slidesigma.com/themes/html/mitor/assets/img/coming-soon/banner-3.jpg'
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+  
 }
